faab995db68c25879bd651f99911605a70416efa
ninjadev999 <ninjadev999@gmail.com>
2019-11-06 12:56:56 -0800

    updated backend url

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,6 +1,6 @@
 export default {
-  server: 'https://www.youtahq.com/',
-  url: 'https://www.youtahq.com/api',
+  server: 'https://youtacalc.herokuapp.com/',
+  url: 'https://youtacalc.herokuapp.com/api',
   formHeaders: {
     'Content-Type':
       'application/json, application/x-www-form-urlencoded; charset=UTF-8'
